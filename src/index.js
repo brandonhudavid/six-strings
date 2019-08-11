bb2df1a60eadc16bb35cdf4833a0e4c8ac729f0e
Brandon David <brandonhudavid@gmail.com>
2019-08-11 13:01:52 -0700

    render songs list with material ui

--- src/index.js
+++ src/index.js
@@ -1,37 +1,96 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-//import './index.css';
-//import App from './App';
-//import * as serviceWorker from './serviceWorker';
+// react
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
 
+// redux
 import { createStore } from 'redux'
 import songsApp from './reducers/reducers'
 import {
 	addSong
 } from './actions/actions'
 
-const store = createStore(songsApp)
+// material ui
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-console.log(store.getState())
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 650,
+  },
+}));
 
-const unsubscribe = store.subscribe(() => console.log(store.getState()))
+class App extends React.Component {
+	
+	constructor(props) {
+		super(props);
+		this.store = createStore(songsApp)
+		console.log(this.store.getState())
+		this.unsubscribe = this.store.subscribe(() => console.log(this.store.getState()))	
+    //this.classes = useStyles()
+	}
 
-// dispatch tests
-store.dispatch(addSong({
-	song: 'Give Love A Try',
-	artist: 'Nick Jonas',
-	difficulty: 'easy',
-	progress: 'finished',
-	resources: 'https://www.youtube.com/watch?v=frxXGvMpgdg'
-}))
-store.dispatch(addSong({
-	song: 'Safe and Sound',
-	artist: 'Taylor Swift',
-	difficulty: 'medium',
-	progress: 'finished',
-	resources: 'https://www.youtube.com/watch?v=Hhdo_mXqltM'
-}))
+	dispatchTests() {
+		// dispatch tests
+		this.store.dispatch(addSong({
+			name: 'Give Love A Try',
+			artist: 'Nick Jonas',
+			difficulty: 'easy',
+			progress: 'finished',
+			resources: 'https://www.youtube.com/watch?v=frxXGvMpgdg'
+		}))
+		this.store.dispatch(addSong({
+			name: 'Safe and Sound',
+			artist: 'Taylor Swift',
+			difficulty: 'medium',
+			progress: 'finished',
+			resources: 'https://www.youtube.com/watch?v=Hhdo_mXqltM'
+		}))
+	}
+	
+	render() {
+		this.dispatchTests()
+		return (
+			<Paper>
+				<Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Song</TableCell>
+              <TableCell align="right">Artist</TableCell>
+              <TableCell align="right">Difficulty</TableCell>
+              <TableCell align="right">Progress</TableCell>
+              <TableCell align="right">Resources</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.store.getState().songs.map(song => (
+              <TableRow key={song.name}>
+                <TableCell component="th" scope="row">
+                  {song.name}
+                </TableCell>
+                <TableCell align="right">{song.artist}</TableCell>
+                <TableCell align="right">{song.difficulty}</TableCell>
+                <TableCell align="right">{song.progress}</TableCell>
+                <TableCell align="right">{song.resources}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+			</Paper>
+		)
+	}
+}
 
-unsubscribe()
-
-//ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
